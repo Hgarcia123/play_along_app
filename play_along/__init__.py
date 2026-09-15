f34dd6ecbb091d1294da7f38462bc8f00e1a8936
Hgarcia123 <hfg004@gmail.com>
2026-09-15 21:20:07 +0100

    Tweaked app creation with blueprint

--- play_along/__init__.py
+++ play_along/__init__.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask
+from . import project
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -7,6 +9,7 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'project.azsqlserver')
     )
+    app.register_blueprint(project.bp)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
--- play_along/project.py
+++ play_along/project.py
@@ -1,44 +1,67 @@
-from flask import (
-    Flask, render_template, request, send_from_directory
-)
-import os
-from yt_dlp import YoutubeDL
 from play_along.db import get_db, init_app
+from play_along.blob import blob_sas_url, send_audio_to_az_blob, delete_audio_from_az_blob
 
-app = Flask(__name__)
+from mssql_python import IntegrityError
 
-AUDIO_DIR = os.path.join(os.path.dirname(__file__), 'audio_files')
+from flask import (
+    Blueprint, render_template, request, flash
+)
+import json
+from dotenv import load_dotenv
 
-init_app(app)
+#YT Downloader
+from yt_dlp import YoutubeDL
 
-@app.route("/", methods=['GET', 'POST'])
+#Load and get env variables
+load_dotenv()
+
+#Global Variables
+from play_along.config import AUDIO_DIR
+
+#Initialize Blueprint
+bp = Blueprint('project', __name__)
+
+
+#ENDPOINTS
+
+@bp.route("/", methods=['GET', 'POST'])
 def main():
-
     if request.method == 'POST':
-        #TODO: Validate URL with regex
+
         url = request.form.get('youtube_url')
 
-        #TODO: Call function get_audiotrack
+        #Call function get_audiotrack
         track_info_dict = get_audiotrack(url)
 
+        #Send audio file to azure blob storage
+        blob_name = send_audio_to_az_blob(track_info_dict.get('title', ""))
+
         #Send info to database
-        send_track_info_to_db(track_info_dict)
+        send_track_info_to_db(track_info_dict, blob_name)
 
     #Query database for track data stored
     track_data = get_track_info_from_db()
-
     return render_template('base.html', track_data=track_data)
 
-@app.route("/wave/<song_name>")
-def wave_audio(song_name):
+@bp.route("/wave/<int:track_id>", methods=['GET', 'POST'])
+def wave_audio(track_id):
+    if request.method == 'POST':
+        regions = json.loads(request.form.get('regions', '[]'))
 
-    _song_name = song_name
+        print(regions)
 
-    return render_template('waveform.html', song_name=_song_name)
+    #Get track info
+    conn = get_db()
+    cursor = conn.cursor()
+  
+    query = f"""SELECT container, blob_name FROM dbo.audio_tracks where id = {track_id}"""
+    cursor.execute(query)
 
-@app.route("/audio/<filename>")
-def serve_audio(filename):
-    return send_from_directory(AUDIO_DIR, filename)
+    track_data = cursor.fetchall()[0]
+
+    sas_url = blob_sas_url(track_data[0], track_data[1])
+    return render_template('waveform.html', url=sas_url)
+
 
 def get_audiotrack(url:str):
     global AUDIO_DIR
@@ -60,34 +83,55 @@ def get_track_info_from_db():
     conn = get_db()
     cursor = conn.cursor()
   
-    query = """SELECT youtube_id, artist, track_name, track_audio, track_album, thumbnail FROM dbo.audio_tracks"""
+    query = """SELECT id, youtube_id, artist, track_name, track_album, thumbnail FROM dbo.audio_tracks"""
     cursor.execute(query)
     track_data = cursor.fetchall()
     cursor.close()
 
     return track_data
 
-def send_track_info_to_db(track_info:dict):
 
-    conn = get_db()
-    cursor = conn.cursor()
-        
-    #Get info from dict
-    youtube_id = track_info.get('id', "")
-    artist = track_info.get('artist', "")
-    track_name = track_info.get('title', "")
-    track_album = track_info.get('album', "")
-    track_audio = "PLACEHOLDER"
-    thumbnail = track_info.get('thumbnail', "")
 
-    query = f"""
-        INSERT INTO audio_tracks (youtube_id, artist, track_name, track_audio, track_album, thumbnail)
-        VALUES(?, ?, ?, ?, ?, ?)
-    """
+def get_regions_from_db(youtube_id:str):
+    ...
 
-    cursor.execute(query, (youtube_id, artist, track_name, track_audio, track_album, thumbnail))
-    conn.commit()
-    cursor.close()
+def send_regions_to_db(regions:json):
+    ...
+
+def send_track_info_to_db(track_info:dict, blob_name:str):
+
+    try:
+        conn = get_db()
+        cursor = conn.cursor()
+            
+        #Get info from dict
+        youtube_id = track_info.get('id', "")
+        artist = track_info.get('artist', "")
+        track_name = track_info.get('title', "")
+        track_album = track_info.get('album', "")
+        thumbnail = track_info.get('thumbnail', "")
+        duration_sec = track_info.get('duration', "")
+
+        #Assign info of blob storage
+        blob_name = blob_name
+        container = "play-along-app-audio-files"
+        content_type = "audio/mpeg"
+
+        query = f"""
+            INSERT INTO audio_tracks (youtube_id, artist, track_name, track_album, thumbnail, duration_sec, blob_name, container, content_type)
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """
+
+        cursor.execute(query, (youtube_id, artist, track_name, track_album, thumbnail, duration_sec, blob_name, container, content_type))
+        conn.commit()
+        cursor.close()
+
+    except IntegrityError as e:
+        flash('Song already exists in DB!', 'error')
+        print(f'Could not save song song to DB with error: {e}')
+
+        #Delete blob that was uploaded to az storage
+        delete_audio_from_az_blob(blob_name=blob_name)
 
 
 if __name__ == '__main__':
